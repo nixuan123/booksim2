47b51cba58fdd541a9988786eee4d307c4e2d8de
nixuan123 <128340967+nixuan123@users.noreply.github.com>
2024-04-27 09:44:39 +0800

    Update hammingmesh.hpp

--- src/networks/hammingmesh.hpp
+++ src/networks/hammingmesh.hpp
@@ -5,26 +5,26 @@
  #include "routefunc.hpp"
 
  class HammingMesh : public Network {
+ void _ComputeSize( const Configuration &config );
+ void _BuildNet( const Configuration &config );
+ 
  public:
  HammingMesh( const Configuration &config, const string & name );
- static void RegisterRoutingFunctions() ;
-
- private:
+ static void RegisterRoutingFunctions(){};
 
  int _a;// 路由器总数
  int _p;// 每个路由器管理的处理节点
  int _k;// 每个路由器的端口数
-
- void _ComputeSize( const Configuration &config );// 计算拓扑大小
- void _BuildNet( const Configuration& config );// 构建网络
- };
-
  int hammingmesh_port(int rID, int src, int dest);
 
  //
  // Routing Functions
  //
  void min_hammingmesh( const Router *r, const Flit *f, int in_channel, OutputSet *outputs, bool inject ) ;
+
+ };
+
+
 
  #endif
 
