aea7fddc3b0c05ef524991b71402eed4d83bf3de
nixuan123 <128340967+nixuan123@users.noreply.github.com>
2024-04-27 15:34:44 +0800

    Update kncube.hpp

--- src/networks/kncube.hpp
+++ src/networks/kncube.hpp
@@ -32,7 +32,7 @@
 
 class KNCube : public Network {
 
-  bool _mesh;
+  bool _mesh;//一个布尔值，表示网络是否是网格状的
 
   int _k;
   int _n;
@@ -40,13 +40,16 @@ class KNCube : public Network {
   void _ComputeSize( const Configuration &config );
   void _BuildNet( const Configuration &config );
 
+  //用于获取网络中节点的邻居通道
   int _LeftChannel( int node, int dim );
   int _RightChannel( int node, int dim );
 
+  //用于获取网络中的邻居节点
   int _LeftNode( int node, int dim );
   int _RightNode( int node, int dim );
 
 public:
+  //公共方法，这些方法可以被类的实例或其他类使用
   KNCube( const Configuration &config, const string & name, bool mesh );
   static void RegisterRoutingFunctions();
 
