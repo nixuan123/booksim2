a10100ce44dfe66a6edc8b427ce2b245ac24adcd
nixuan123 <128340967+nixuan123@users.noreply.github.com>
2024-05-07 15:06:31 +0800

    Update hammingmesh.hpp

--- src/networks/hammingmesh.hpp
+++ src/networks/hammingmesh.hpp
@@ -25,75 +25,304 @@
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-////////////////////////////////////////////////////////////////////////
-//
-// CMesh: Mesh topology with concentration and express links along the
-//         edge of the network
-//
-////////////////////////////////////////////////////////////////////////
-//
-// RCS Information:
-//  $Author: jbalfour $
-//  $Date: 2007/06/26 22:49:23 $
-//  $Id$
-// 
-////////////////////////////////////////////////////////////////////////
-#ifndef _HAMMINGMESH_HPP_
-#define _HAMMINGMESH_HPP_
-//包含了网络和函数的声明
-#include "network.hpp"
-#include "routefunc.hpp"
-//定义了一个HammingMesh的网络拓扑，它继承自Network类
-class HammingMesh : public Network {
-public:
-  //使用配置信息和网络名称来初始化CMesh实例
-  HammingMesh( const Configuration &config, const string & name );
-  int GetN() const;//用于返回网络的维度
-  int GetK() const;//用于返回每个维度上路由器的数量
+/*kn.cpp
+ *
+ *Meshs, cube, torus
+ *
+ */
 
-  static int IdToLocation( int route_id);//将路由器id转换为在拓扑中的位置
-  static int NodeToRouter( int address ) ;//将节点地址转换为路由器id
-  static int NodeToPort( int address ) ;//将节点地址转换为端口号
-
-  static void RegisterRoutingFunctions() ;//注册路由函数
-
-private:
+#include "booksim.hpp"
+#include <vector>
+#include <sstream>
+#include <ctime>
+#include <cassert>
+#include "kncube.hpp"
+#include "random_utils.hpp"
+#include "misc_utils.hpp"
+ //#include "iq_router.hpp"
 
 
-  void _ComputeSize( const Configuration &config );//根据配置信息计算网络的大小
-  void _BuildNet( const Configuration& config );//构建网络，包括初始化路由器和通道
+KNCube::KNCube( const Configuration &config, const string & name, bool mesh ) :
+Network( config, name )
+{
+  _mesh = mesh;
 
-  int a ;
-  int b ;
-  int x ;
-  int y ;
+  _ComputeSize( config );
+  _Alloc( );
+  _BuildNet( config );
+}
+//一个主机，每个维度上有两条输出通道和输入通道
+void KNCube::_ComputeSize( const Configuration &config )
+{
+  _k = config.GetInt( "k" );
+  _n = config.GetInt( "n" );
 
-//前两个是板内的维度大小，后面两个是hm/板间维度的大小
-  int* dim_size;
-  int* dim_width;
+  gK = _k; gN = _n;
+  _size     = powi( _k, _n );
+  _channels = 2*_n*_size;
+
+  _nodes = _size;
+}
+
+void KNCube::RegisterRoutingFunctions() {
+
+}
+void KNCube::_BuildNet( const Configuration &config )
+{
+  int left_node;
+  int right_node;
+
+  int right_input;
+  int left_input;
+
+  int right_output;
+  int left_output;
+  //创建了一个ostringstream对象，它是一个能将输出流定向到一个字符串的类
+  ostringstream router_name;
+
+  //latency type, noc or conventional network
+  bool use_noc_latency;
+  use_noc_latency = (config.GetInt("use_noc_latency")==1);
+  
+  for ( int node = 0; node < _size; ++node ) {
+    //将字符串"router"插入到router_name对象中
+    router_name << "router";
     
-  int (* port_start)[2]; // port_start[dim][direction: 0=pos, 1=neg]
-  int num_local_ports;//每个路由器需要连接的主机端口数量
-  int *num_switch_ports;//【新增】行、列交换机分别需要的端口数量
-  int hm_id;//【新增】路由器的hm板id
-  int local_port_start;
+    if ( _k > 1 ) {
+      for ( int dim_offset = _size / _k; dim_offset >= 1; dim_offset /= _k ) {
+	router_name << "_" << ( node / dim_offset ) % _k;
+      }
+    }
+    //2*_n+1代表路由器的出度和入度
+    _routers[node] = Router::NewRouter( config, this, router_name.str( ), 
+					node, 2*_n + 1, 2*_n + 1 );
+    _timed_modules.push_back(_routers[node]);
 
-};
+    router_name.str("");
 
-//
-// Routing Functions
-//
-//路由函数
-void xy_yx_hammingmesh( const Router *r, const Flit *f, int in_channel, 
-		  OutputSet *outputs, bool inject ) ;
+    for ( int dim = 0; dim < _n; ++dim ) {
 
-void xy_yx_no_express_hammingmesh( const Router *r, const Flit *f, int in_channel, 
-			     OutputSet *outputs, bool inject ) ;
-//实现了基于XY-YX路由策略的路由函数，前者考虑了快速通道，后者没有
-void dor_hammingmesh( const Router *r, const Flit *f, int in_channel, 
-		OutputSet *outputs, bool inject ) ;
+      //find the neighbor 
+      left_node  = _LeftNode( node, dim );
+      right_node = _RightNode( node, dim );
 
-void dor_no_express_cmesh( const Router *r, const Flit *f, int in_channel, 
-			   OutputSet *outputs, bool inject ) ;
+      //
+      // Current (N)ode
+      // (L)eft node
+      // (R)ight node
+      //
+      //   L--->N<---R
+      //   L<---N--->R
+      //
 
-#endif
+      // torus channel is longer due to wrap around
+      int latency = _mesh ? 1 : 2 ;
+
+      //get the input channel number
+      right_input = _LeftChannel( right_node, dim );
+      left_input  = _RightChannel( left_node, dim );
+
+      //add the input channel
+      _routers[node]->AddInputChannel( _chan[right_input], _chan_cred[right_input] );
+      _routers[node]->AddInputChannel( _chan[left_input], _chan_cred[left_input] );
+
+      //set input channel latency
+      if(use_noc_latency){
+	_chan[right_input]->SetLatency( latency );
+	_chan[left_input]->SetLatency( latency );
+	_chan_cred[right_input]->SetLatency( latency );
+	_chan_cred[left_input]->SetLatency( latency );
+      } else {
+	_chan[left_input]->SetLatency( 1 );
+	_chan_cred[right_input]->SetLatency( 1 );
+	_chan_cred[left_input]->SetLatency( 1 );
+	_chan[right_input]->SetLatency( 1 );
+      }
+      //get the output channel number
+      right_output = _RightChannel( node, dim );
+      left_output  = _LeftChannel( node, dim );
+      
+      //add the output channel
+      _routers[node]->AddOutputChannel( _chan[right_output], _chan_cred[right_output] );
+      _routers[node]->AddOutputChannel( _chan[left_output], _chan_cred[left_output] );
+
+      //set output channel latency
+      if(use_noc_latency){
+	_chan[right_output]->SetLatency( latency );
+	_chan[left_output]->SetLatency( latency );
+	_chan_cred[right_output]->SetLatency( latency );
+	_chan_cred[left_output]->SetLatency( latency );
+      } else {
+	_chan[right_output]->SetLatency( 1 );
+	_chan[left_output]->SetLatency( 1 );
+	_chan_cred[right_output]->SetLatency( 1 );
+	_chan_cred[left_output]->SetLatency( 1 );
+
+      }
+    }
+    //injection and ejection channel, always 1 latency
+    _routers[node]->AddInputChannel( _inject[node], _inject_cred[node] );
+    _routers[node]->AddOutputChannel( _eject[node], _eject_cred[node] );
+    _inject[node]->SetLatency( 1 );
+    _eject[node]->SetLatency( 1 );
+  }
+}
+
+int KNCube::_LeftChannel( int node, int dim )
+{
+  // The base channel for a node is 2*_n*node
+  int base = 2*_n*node;
+  // The offset for a left channel is 2*dim + 1
+  int off  = 2*dim + 1;
+
+  return ( base + off );
+}
+
+int KNCube::_RightChannel( int node, int dim )
+{
+  // The base channel for a node is 2*_n*node
+  int base = 2*_n*node;
+  // The offset for a right channel is 2*dim 
+  int off  = 2*dim;
+  return ( base + off );
+}
+//这个函数是找寻该路由器在dim维度中的左邻居路由器节点
+int KNCube::_LeftNode( int node, int dim )
+{
+  //假设是3维立方体结构
+  //一层有多少户"人口"
+  int k_to_dim = powi( _k, dim );
+  //该住户在第"几层"
+  int loc_in_dim = ( node / k_to_dim ) % _k;
+  int left_node;
+  // if at the left edge of the dimension, wraparound
+  if ( loc_in_dim == 0 ) {
+    left_node = node + (_k-1)*k_to_dim;
+  } else {
+    left_node = node - k_to_dim;
+  }
+
+  return left_node;
+}
+
+int KNCube::_RightNode( int node, int dim )
+{
+  int k_to_dim = powi( _k, dim );
+  int loc_in_dim = ( node / k_to_dim ) % _k;
+  int right_node;
+  // if at the right edge of the dimension, wraparound
+  if ( loc_in_dim == ( _k-1 ) ) {
+    right_node = node - (_k-1)*k_to_dim;
+  } else {
+    right_node = node + k_to_dim;
+  }
+
+  return right_node;
+}
+
+int KNCube::GetN( ) const
+{
+  return _n;
+}
+
+int KNCube::GetK( ) const
+{
+  return _k;
+}
+
+/*legacy, not sure how this fits into the new scheme of things*/
+void KNCube::InsertRandomFaults( const Configuration &config )
+{
+  int num_fails = config.GetInt( "link_failures" );
+  
+  if ( _size && num_fails ) {
+    vector<long> save_x;
+    vector<double> save_u;
+    SaveRandomState( save_x, save_u );
+    int fail_seed;
+    if ( config.GetStr( "fail_seed" ) == "time" ) {
+      fail_seed = int( time( NULL ) );
+      cout << "SEED: fail_seed=" << fail_seed << endl;
+    } else {
+      fail_seed = config.GetInt( "fail_seed" );
+    }
+    RandomSeed( fail_seed );
+
+    vector<bool> fail_nodes(_size);
+
+    for ( int i = 0; i < _size; ++i ) {
+      int node = i;
+
+      // edge test
+      bool edge = false;
+      for ( int n = 0; n < _n; ++n ) {
+	if ( ( ( node % _k ) == 0 ) ||
+	     ( ( node % _k ) == _k - 1 ) ) {
+	  edge = true;
+	}
+	node /= _k;
+      }
+
+      if ( edge ) {
+	fail_nodes[i] = true;
+      } else {
+	fail_nodes[i] = false;
+      }
+    }
+
+    for ( int i = 0; i < num_fails; ++i ) {
+      int j = RandomInt( _size - 1 );
+      bool available = false;
+      int node = -1;
+      int chan = -1;
+      int t;
+
+      for ( t = 0; ( t < _size ) && (!available); ++t ) {
+	int node = ( j + t ) % _size;
+       
+	if ( !fail_nodes[node] ) {
+	  // check neighbors
+	  int c = RandomInt( 2*_n - 1 );
+
+	  for ( int n = 0; ( n < 2*_n ) && (!available); ++n ) {
+	    chan = ( n + c ) % 2*_n;
+
+	    if ( chan % 1 ) {
+	      available = fail_nodes[_LeftNode( node, chan/2 )];
+	    } else {
+	      available = fail_nodes[_RightNode( node, chan/2 )];
+	    }
+	  }
+	}
+	
+	if ( !available ) {
+	  cout << "skipping " << node << endl;
+	}
+      }
+
+      if ( t == _size ) {
+	Error( "Could not find another possible fault channel" );
+      }
+
+      assert(node != -1);
+      assert(chan != -1);
+      OutChannelFault( node, chan );
+      fail_nodes[node] = true;
+
+      for ( int n = 0; ( n < _n ) && available ; ++n ) {
+	fail_nodes[_LeftNode( node, n )]  = true;
+	fail_nodes[_RightNode( node, n )] = true;
+      }
+
+      cout << "failure at node " << node << ", channel " 
+	   << chan << endl;
+    }
+
+    RestoreRandomState( save_x, save_u );
+  }
+}
+
+double KNCube::Capacity( ) const
+{
+  return (double)_k / ( _mesh ? 8.0 : 4.0 );
+}
